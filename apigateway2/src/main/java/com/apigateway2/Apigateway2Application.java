f857a53f93c50892cbeb15be663e53d62858586f
Hrishikesh <hrishisw10@gmail.com>
2024-07-31 10:55:49 +0530

    API Gateway to route the user request to specific service

---
+++ apigateway2/src/main/java/com/apigateway2/Apigateway2Application.java
@@ -0,0 +1,13 @@
+package com.apigateway2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apigateway2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apigateway2Application.class, args);
+	}
+
+}
